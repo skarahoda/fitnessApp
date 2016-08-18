82896f552206fae5ab99ee73c7313e26f285c03e
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2016-08-18 18:40:45 +0300

    android user info

--- routes/android.js
+++ routes/android.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var isLoggedIn = require('./isLoggedIn');
+var User = require('../models/user');
 
 /* GET home page. */
-router.post('/hello', function (req, res) {
-	res.status('200').json({hello: 'world'});
+router.post('/user', function (req, res) {
+	User.findById(id, function (err, user) {
+		if (err)
+			res.status('400').json({});
+		else {
+			res.status('200').json(user);
+		}
+	});
+
 });
 
 module.exports = router;
