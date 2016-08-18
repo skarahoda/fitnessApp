23ad29fa65f43db4654affcba2cbf9b47be7a6ec
EC2 Default User <ec2-user@ip-172-30-0-125.us-west-2.compute.internal>
2016-08-18 17:01:19 +0000

    android post requests are improved

--- routes/android.js
+++ routes/android.js
@@ -3,13 +3,42 @@ var router = express.Router();
 var isLoggedIn = require('./isLoggedIn');
 var User = require('../models/user');
 
-/* GET home page. */
+
 router.post('/user', function (req, res) {
-	User.findById(id, function (err, user) {
+	User.findById(req.body.id, function (err, user) {
 		if (err)
 			res.status('400').json({});
 		else {
-			res.status('200').json(user);
+			res.status('200').json({
+				name: user.name,
+				weight: user.weight,
+				height: user.height
+			});
+		}
+	});
+
+});
+
+router.post('/updateUser', function (req, res) {
+	User.findById(req.body.id, function (err, user) {
+		if (err)
+			res.status('400').json({});
+		else {
+			user.name = req.body.name || user.name ;
+			user.weight = req.body.weight || user.weight ;
+			user.height = req.body.height || user.height ;
+			user.save(function (err, user) {
+				if (err) {
+					res.status(500).send(err)
+				}
+				res.status(200).json({
+						name: user.name,
+						id: user.id,
+						weight: user.weight,
+						height: user.height,
+						isGoogleConnected: (user.google.id != null)
+					})
+			});
 		}
 	});
 
