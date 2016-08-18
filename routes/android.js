8def6c873cc49cea4fc7c6edf4f6f6d0d69f34b5
berkcirisci <berkcirisci@sabanciuniv.edu>
2016-08-18 16:23:00 +0300

    android

--- routes/android.js
+++ routes/android.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var isLoggedIn = require('./isLoggedIn');
 
 /* GET home page. */
-router.get('/hello', function (req, res) {
+router.post('/hello', function (req, res) {
 	res.status('200').json({hello: 'world'});
 });
 
