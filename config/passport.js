27946a436abc615e9f0401da7e685afd85f3e980
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2016-08-18 18:31:08 +0300

    login for android is deleted

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,6 @@
 // load all the things we need
 var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth2').Strategy;
-var LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
 var User = require('../models/user');
@@ -25,36 +24,6 @@ module.exports = function (passport) {
 			done(err, user);
 		});
 	});
-
-	// =========================================================================
-	// LOCAL LOGIN =============================================================
-	// =========================================================================
-	// we are using named strategies since we have one for login and one for signup
-	// by default, if there was no name, it would just be called 'local'
-
-	passport.use('local-login', new LocalStrategy({
-			// by default, local strategy uses username and password, we will override with email
-			idField: 'id',
-			passReqToCallback: true // allows us to pass back the entire request to the callback
-		},
-		function (req, id, done) { // callback with email and password from our form
-
-			// find a user whose email is the same as the forms email
-			// we are checking to see if the user trying to login already exists
-			User.findOne({'id': id}, function (err, user) {
-				// if there are any errors, return the error before anything else
-				if (err)
-					return done(err);
-
-				// if no user is found, return the message
-				if (!user)
-					return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-
-				// all is well, return successful user
-				return done(null, user);
-			});
-
-		}));
 
 	// =========================================================================
 	// FACEBOOK ================================================================
